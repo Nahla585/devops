53a82870f0260d4b997056414d5f54b3038608f4
Nahla585 <142793695+Nahla585@users.noreply.github.com>
2023-12-04 00:08:34 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const appointmentRoute=require("./routes/appointment.js")
+const mongoose=require("mongoose")
+const app=express();
+const dotenv=require("dotenv")
+const cors = require("cors");
+app.use(cors());
+
+dotenv.config()
+
+app.use(express.json())
+
+//app.use(express.json())
+
+const connect = async()=>{
+    await mongoose.connect(process.env.db);
+    console.log("Connected with database !!! ")
+};
+
+
+app.use("/api",appointmentRoute)
+
+app.get('/vs',(req,res)=>{
+    res.json('VS Endpoint');
+});
+
+
+
+
+app.get('/test',(req,res)=>{
+    res.json('Test');
+});
+
+app.get('/',(req,res)=>{
+    res.json('Start Endpoint');
+});
+
+
+
+
+
+app.listen(3000,()=>{
+    connect()
+    console.log("connected with succes to backend")
+})
